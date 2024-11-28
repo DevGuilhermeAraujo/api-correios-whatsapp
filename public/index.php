bfa8ad5994bd92fff82f7fabd485be04b6a66b5a
DevGuilhermeAraujo <tecgui.informatica@gmail.com>
2024-11-28 15:02:29 -0300

    Criação da página inicial index.php

---
+++ public/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Página Inicial</title>
+    <!-- CSS Externo -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <!-- CSS Local -->
+    <link rel="stylesheet" href="assets/css/styles.css">
+</head>
+
+<body>
+    <div class="container mt-5">
+        <h1>Página Inicial</h1>
+        <p>Esta página vai rodar a função da API imediatamente e depois a cada 15 minutos.</p>
+    </div>
+
+    <!-- JavaScript Externo -->
+    <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+    <!-- JavaScript Local -->
+    <script src="assets/js/scripts.js"></script>
+</body>
+
+</html>
